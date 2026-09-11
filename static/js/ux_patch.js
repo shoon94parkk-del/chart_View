6baaf3087dd5f2a658a8604bcca088afbf5e042c
shoon94parkk-del <shoon94.parkk@gmail.com>
2026-09-11 11:06:24 +0900

    fix: load higher-contrast bottom navigation styles

--- static/js/ux_patch.js
+++ static/js/ux_patch.js
@@ -29,6 +29,7 @@
   ensureStyle('link[data-ux-v3]', '/static/css/ux_v3.css?v=20260911v4', 'uxV3');
   ensureScript('script[data-revision-radar]', '/static/js/revision_radar.js?v=20260911v3', 'revisionRadar');
   ensureScript('script[data-ux-v3]', '/static/js/ux_v3.js?v=20260911v4', 'uxV3');
+  ensureStyle('link[data-bottom-nav-v6]', '/static/css/bottom_nav_v6.css?v=20260911v6', 'bottomNavV6');
   ensureStyle('link[data-app-header-v5]', '/static/css/app_header_v5.css?v=20260911v5', 'appHeaderV5');
   ensureScript('script[data-app-header-v5]', '/static/js/app_header_v5.js?v=20260911v5', 'appHeaderV5');
 
