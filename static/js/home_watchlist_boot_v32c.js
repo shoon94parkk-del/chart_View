db2d9a36d80eebb30504c4693a6ea9d6646111bd
shoon94parkk-del <shoon94.parkk@gmail.com>
2026-09-13 11:47:06 +0900

    Load V40.1 news context styles

--- static/js/home_watchlist_boot_v32c.js
+++ static/js/home_watchlist_boot_v32c.js
@@ -36,6 +36,7 @@
     addStyle('link[data-release-ui-v40]', '/static/css/release_ui_v40.css?v=20260913stage12', 'releaseUiV40');
     addStyle('link[data-detail-ui-v40]', '/static/css/detail_ui_v40.css?v=20260913stage12', 'detailUiV40');
     addStyle('link[data-comparison-ui-v40]', '/static/css/comparison_ui_v40.css?v=20260913stage12', 'comparisonUiV40');
+    addStyle('link[data-news-status-v401]', '/static/css/news_status_v40_1.css?v=20260913v401', 'newsStatusV401');
     addScript('script[data-app-state-v40]', '/static/js/app_state_v40.js?v=20260913stage12', 'appStateV40');
     addScript('script[data-single-detail-v40]', '/static/js/single_detail_v40.js?v=20260913stage12', 'singleDetailV40');
     addScript('script[data-personalized-news-v40]', '/static/js/personalized_news_v40.js?v=20260913stage12', 'personalizedNewsV40');
