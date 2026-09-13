6539ebf1d917b3e409a7b9d7edcdf87b95f3b104
shoon94parkk-del <shoon94.parkk@gmail.com>
2026-09-13 12:18:34 +0900

    V41: load MY hub assets

--- static/js/home_watchlist_boot_v32c.js
+++ static/js/home_watchlist_boot_v32c.js
@@ -38,6 +38,7 @@
     addStyle('link[data-comparison-ui-v40]', '/static/css/comparison_ui_v40.css?v=20260913stage12', 'comparisonUiV40');
     addStyle('link[data-comparison-compact-v401]', '/static/css/comparison_compact_v40_1.css?v=20260913v401', 'comparisonCompactV401');
     addStyle('link[data-news-status-v401]', '/static/css/news_status_v40_1.css?v=20260913v401', 'newsStatusV401');
+    addStyle('link[data-my-hub-v41]', '/static/css/my_hub_v41.css?v=20260913v41', 'myHubV41');
     addScript('script[data-app-state-v40]', '/static/js/app_state_v40.js?v=20260913stage12', 'appStateV40');
     addScript('script[data-single-detail-v40]', '/static/js/single_detail_v40.js?v=20260913stage12', 'singleDetailV40');
     addScript('script[data-detail-visibility-v401]', '/static/js/detail_visibility_v40_1.js?v=20260913v401', 'detailVisibilityV401');
@@ -46,6 +47,7 @@
     addScript('script[data-release-ui-v40]', '/static/js/release_ui_v40.js?v=20260913stage12', 'releaseUiV40');
     addScript('script[data-release-flow-v40]', '/static/js/release_flow_v40.js?v=20260913stage12', 'releaseFlowV40');
     addScript('script[data-comparison-compact-v401]', '/static/js/comparison_compact_v40_1.js?v=20260913v401', 'comparisonCompactV401');
+    addScript('script[data-my-hub-v41]', '/static/js/my_hub_v41.js?v=20260913v41', 'myHubV41');
   }
 
   function ensureAllAssets() {
@@ -101,9 +103,9 @@
 
     home.dataset.homeOrder = 'market-watchlist-news-body-status';
     home.dataset.newsVersion = 'v40';
-    home.dataset.newsPatch = 'v40.1';
+    home.dataset.newsPatch = 'v41';
     home.dataset.visualVersion = 'v39';
-    home.dataset.uiVersion = 'v40-stage12';
+    home.dataset.uiVersion = 'v41-my-hub';
     return Boolean(market && body);
   }
 
