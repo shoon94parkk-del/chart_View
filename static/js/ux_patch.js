17ee4e406876b2e3730d16c9441efca3f0ff8a79
shoon94parkk-del <shoon94.parkk@gmail.com>
2026-09-14 12:02:44 +0900

    feat: load watchlist quick-add action

--- static/js/ux_patch.js
+++ static/js/ux_patch.js
@@ -32,6 +32,7 @@
   ensureScript('script[data-investment-tools-v22]', '/static/js/investment_tools_v22.js?v=20260912v28', 'investmentToolsV22');
   ensureStyle('link[data-watchlist-v30]', '/static/css/watchlist_v30.css?v=20260912v33', 'watchlistV30');
   ensureScript('script[data-watchlist-v30]', '/static/js/watchlist_v30.js?v=20260912audit1', 'watchlistV30');
+  ensureScript('script[data-watchlist-quick-add-v48]', '/static/js/watchlist_quick_add_v48.js?v=20260914v48', 'watchlistQuickAddV48');
   ensureScript('script[data-ux-v3]', '/static/js/ux_v3.js?v=20260913v43nav1', 'uxV3');
   ensureStyle('link[data-bottom-nav-v6]', '/static/css/bottom_nav_v6.css?v=20260912v32e', 'bottomNavV6');
   ensureStyle('link[data-app-states-v32]', '/static/css/app_states_v32.css?v=20260912v32', 'appStatesV32');
