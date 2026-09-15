ee310a350dac32549b9d9921e23c33086eeb8649
User <user@example.com>
2026-09-15 20:30:47 +0900

    fix: bust homepage AI ranking widget cache

--- static/js/promo_v1.js
+++ static/js/promo_v1.js
@@ -53,7 +53,7 @@
       return;
     }
     const script = document.createElement('script');
-    script.src = '/static/js/ai_daily_widget.js?v=20260914v7';
+    script.src = '/static/js/ai_daily_widget.js?v=20260915v9';
     script.async = false;
     script.dataset.aiDailyHome = '1';
     script.dataset.version = 'v6';
